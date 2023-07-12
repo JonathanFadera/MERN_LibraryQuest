6c529ced41d889272c8c3f1fb3ac791152a98a8f
AlbusSeverus <ericsonhnc@gmail.com>
2023-07-11 21:10:14 -0700

    debug v13

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 module.exports = mongoose.connection;
