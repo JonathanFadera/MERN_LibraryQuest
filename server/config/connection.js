b4f6925e52ecebae025beacc44c53b4deb70cf91
AlbusSeverus <ericsonhnc@gmail.com>
2023-07-11 19:54:35 -0700

    Update MongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-module.exports = mongoose.connection;
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
